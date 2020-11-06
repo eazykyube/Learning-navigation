e60bb293597be3664202fb2fb2b71aa149759e3c
Enes Ayan <eayan2012@litsey2.ru>
2020-11-07 00:32:32 +0300

    - Changed RegisterFragment to NameFragment

---
+++ app/src/main/java/com/example/lab_1/NameFragment.kt
@@ -0,0 +1,41 @@
+package com.example.lab_1
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.lab_1.databinding.FragmentRegisterBinding
+
+class NameFragment : Fragment() {
+
+    lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_register, container, false
+        )
+
+        binding.nextButton.setOnClickListener {
+            checkRegister(it)
+        }
+
+        return binding.root
+    }
+
+    private fun checkRegister(view: View) {
+        binding.apply {
+            if (TextUtils.isEmpty(binding.nameEditReg.text.toString())) {
+                Toast.makeText(context, "Please, enter your shit above", Toast.LENGTH_SHORT).show();
+            } else {
+                // TODO: Move data from the input to livedata and show it in reg_text_res
+            }
+        }
+    }
+}
