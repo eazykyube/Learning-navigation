502811548c07bc74a7f8e20ddf095d8128e60852
Enes Ayan <eayan2012@litsey2.ru>
2020-11-06 23:58:53 +0300

    Structure changes: - Added two fragments - Using them in MainActivity.kt - This is done via navigation(see res/navigation)
    Still pretty empty. Need to save data and make RegisterFragment with steps, in one or separate fragments, preferably first option, but looks like second one is required(why? it sucks)

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -15,42 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding.loginButton.setOnClickListener {
-            checkLogin(it)
-        }
-
-        binding.logoutButton.setOnClickListener {
-            logout(it)
-        }
     }
 
-    private fun checkLogin(view: View) {
-        binding.apply {
-            if (TextUtils.isEmpty(binding.nameEdit.text.toString()) or TextUtils.isEmpty(binding.pwdEdit.text.toString())) {
-                Toast.makeText(applicationContext, "Please, enter your shit above", Toast.LENGTH_SHORT).show();
-            } else {
-                binding.nameEdit.text.clear()
-                binding.pwdEdit.text.clear()
-                binding.nameEdit.visibility = View.GONE
-                binding.pwdEdit.visibility = View.GONE
-                binding.loginButton.visibility = View.GONE
-                binding.registerText.visibility = View.GONE
-                invalidateAll()
-                binding.logoutButton.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun logout(view: View) {
-        binding.apply {
-            binding.logoutButton.visibility = View.GONE
-            invalidateAll()
-                binding.nameEdit.visibility = View.VISIBLE
-                binding.pwdEdit.visibility = View.VISIBLE
-                binding.loginButton.visibility = View.VISIBLE
-                binding.registerText.visibility = View.VISIBLE
-        }
-    }
 }
